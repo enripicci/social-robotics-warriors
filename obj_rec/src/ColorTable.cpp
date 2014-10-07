1f293ba804cd4fe58c9e52d6034bf967cf16d95c
enripicci <picci91@tiscali.it>
2014-10-07 14:56:50 +0200

    ColorTable class commentata e revisionata

--- obj_rec/src/ColorTable.cpp
+++ obj_rec/src/ColorTable.cpp
@@ -1,26 +1,37 @@
+#include <cstdlib>  // ensure we have malloc
 #include "ColorTable.h"
 
-double dist_sq( double *a1, double *a2, int dims ) {
-  double dist_sq = 0, diff;
-  while( --dims >= 0 ) {
-    diff = (a1[dims] - a2[dims]);
-    dist_sq += diff*diff;
+using namespace std;
+
+// direct access to pixel (_x,_y) of image _img
+#define PIXEL(_img,_x,_y)   (                                                       \
+    &((uchar*)(_img->imageData + _img->widthStep *                                  \
+            ( (_y < 0) ? (0) : ( (_y >= _img->height) ? (_img->height-1) : (_y) ) ) \
+    )) [ ( (_x < 0)? (0) : ( (_x >= _img->width) ?( _img->width -1) : (_x))) * _img->nChannels ]    \
+  )
+
+
+/* Given two vectors a1 and a2, each of dimension dims,
+*  return the 
+*/
+static double dist_sq( double *a1, double *a2, int dims ) {
+  double dist = 0, diff;
+  for(--dims; dims>=0; dims--) {
+    diff = a1[dims] - a2[dims];
+    dist += diff*diff;
   }
-  return dist_sq;
+  return dist;
 }
 
 
-ColorTable::ColorTable()
-{
+ColorTable::ColorTable() {
 	// Let's create the tree
 	tree = kd_create(3);
-
 	kd_data_destructor( tree, free);
 }
 
 
-ColorTable::~ColorTable()
-{
+ColorTable::~ColorTable() {
 	colorTable.clear();
 
 	kd_clear(tree);
@@ -28,38 +39,32 @@ ColorTable::~ColorTable()
 	tree = 0;
 }
 
-void ColorTable::clearSamples()
-{
-	kd_clear(tree);
-}
-
-void ColorTable::clearSpecifics()
-{
-	colorTable.clear();
-}
-
-
-ColorClass ColorTable::getColorClass( CvScalar color)
-{
-	double radius = 10.0;
+ColorClass ColorTable::getColorClass( CvScalar color, double radius) {
 
 	ColorClass retVal = 0;
+	ColorClass *c;
+	double dist;
+	double pos[3];
 
 	/* find points closest to the origin and within distance radius */
 	struct kdres * presults = kd_nearest_range( tree, color.val, radius);
-
-	while( !kd_res_end( presults ) )
-	{
-		double pos[3];
-
-		// get the data and position of the current result item
-		ColorClass * c = (ColorClass *) kd_res_item( presults, pos);
+	
+	if(NULL==presults) {
+        // something went wrong
+        /// TODO: return 0 is a good error value?
+        return 0;
+    }
+    
+    
+    // iterate all the results
+	while( NULL != kd_res_end( presults ) ) {
+		// get the data and position of the current result item. c should never be NULL
+		c = (ColorClass *) kd_res_item( presults, pos);
 		
 		// compute the distance of the current result from the pt
-		double dist = sqrt( dist_sq( color.val, pos, 3 ) );
+		dist = sqrt( dist_sq( color.val, pos, 3 ) );
 
-		if( dist <= radius)
-		{
+		if( dist <= radius) {
 			retVal = *c;
 			radius = dist;
 		}
@@ -74,16 +79,42 @@ ColorClass ColorTable::getColorClass( CvScalar color)
 }
 
 
-bool ColorTable::addSample( ColorClass colorClass, CvScalar color)
-{
-	ColorClass *c = new ColorClass; *c = colorClass;
-	kd_insert( tree, color.val, c);
-	return true;
+bool ColorTable::addSample( ColorClass colorClass, CvScalar color) {
+    int res;
+    // malloc is required because the deallocator of the tree is free()
+	ColorClass *c = static_cast<ColorClass*>( malloc(sizeof(ColorClass)) );
+	
+	if(NULL==c)
+	    return false;
+	    
+	*c = colorClass;
+	
+	// Insert the color class, ordered by color value
+	res = kd_insert( tree, color.val, c);
+	return (0==res)?(true):(false);
+}
+
+void ColorTable::clearSamples() {
+	kd_clear(tree);
 }
 
 
+void ColorTable::setColorClassSpecifics( ColorClass id, CvScalar color, bool seg) {
+    ColorClassSpecifics c;
+    c.seg = seg;
+    c.b = (int) color.val[0];
+    c.g = (int) color.val[1];
+    c.r = (int) color.val[2];
+
+    colorTable[id] = c;
+}
+
+void ColorTable::clearSpecifics() {
+	colorTable.clear();
+}
+
 /*
-bool ColorTable::load( string filename)
+bool ColorTable::load( string filename, unsigned int mergemod)
 {
 	//colorTable.clear();
 
@@ -122,25 +153,36 @@ bool ColorTable::load( string filename)
 
 	return true;
 }
+
+bool loadTable(const char* filename, unsigned int mergemod) {
+
+}
 */
 
-int ColorTable::segment( const IplImage * in, const IplImage * out, int d_x, int d_y)
-{
+int ColorTable::segment( const IplImage * in, const IplImage * out, int d_x, int d_y) {
+    register int i;
+    register int j;
+    uchar *pIn, *pOut;
+    ColorClass c;
+    ColorClassSpecifics cSpec;
 //	IplImage * hsv = cvCreateImage( cvSize(in->width, in->height), in->depth, in->nChannels);
 //	cvCvtColor( in, hsv, CV_BGR2HSV);
 
-	for( int i = 0; i < in->width; i += d_x)
-	for( int j = 0; j < in->height; j += d_y)
-	{
-		uchar * pIn = PIXEL( in, i, j);
-		uchar * pOut = PIXEL( out, i, j);
+	for(i=0; i < in->width; i+=d_x) {
+	    for(j=0; j < in->height; j+=d_y) {
+		    pIn = PIXEL( in, i, j);
+		    pOut = PIXEL( out, i, j);
 
-		ColorClass c = getColorClass(cvScalar(pIn[0], pIn[1], pIn[2]));
+		    c = getColorClass(cvScalar(pIn[0], pIn[1], pIn[2]));
+		    
+		    /// TODO: check the return value of c
+		    cSpec = colorTable[c];
 
-		pOut[0] = colorTable[c].b;
-		pOut[1] = colorTable[c].g;
-		pOut[2] = colorTable[c].r;
-	}
+		    pOut[0] = cSpec.b;
+		    pOut[1] = cSpec.g;
+		    pOut[2] = cSpec.r;
+		}
+    }
 
 //	cvReleaseImage(&hsv);
 
--- obj_rec/src/ColorTable.h
+++ obj_rec/src/ColorTable.h
@@ -3,66 +3,96 @@
 
 #include <map>
 #include <string>
-using namespace std;
-
 // OpenCV Lib
 #include <cv.h>
 #include <cxcore.h>
 
 #include "kdtree.h"
 
-//direct pixel access
-#define PIXEL(_img,_x,_y) (&((uchar*)(_img->imageData+ _img->widthStep* ( (_y < 0)? 0: ((_y >= _img->height)? _img->height -1: _y)) ))[ ( (_x < 0)? 0: ((_x >= _img->width)? _img->width -1: _x)) *_img->nChannels])
+using namespace std;
 
-
+// id associated to a sample
 typedef int ColorClass;
 
-class ColorTable
-{
-public:
-	ColorTable();
-	virtual ~ColorTable();
+/* This class assolve to two works:
+*    1) Store the color samples, indexing them with an id.
+*       The value zero is reserved, and the uni of the ids is left to the user.
+*    2) Store the color map used to map the samples ids into quantized colors.
+*/
+class ColorTable {
+    public:
+	    ColorTable();
+	    virtual ~ColorTable();
+	    
+	    /**   Samples manager
+	    **/
+        /* Return the ColorClass that suite better the supplied color.
+        *  radius is the treshold inside which the research is done.
+        *  Return 0 on error, the color class otherwise
+        */
+        /// TODO: check if the return value for error is usable for that scope
+	    ColorClass getColorClass( const CvScalar color, double radius = 10.0);
+	    
+	    /* Add a new sample with id colorClass and color color, RGB coded.
+	    *  Return false on error, true otherwise.
+	    */
+	    bool addSample( const ColorClass colorClass, const CvScalar color);
+	    
+	    /* Clear all the samples, and delete them from memory
+	    */
+	    void clearSamples();
+	    
+	    /**   Color map manager
+	    **/
+        /* Map the sample with specified id to the specified color.
+        */
+	    void setColorClassSpecifics( ColorClass id, CvScalar color, bool seg = true);
+	    
+	    /* Clear the color map
+	    */
+	    void clearSpecifics();
+	    
+	    /* Take the pixel of the input in image (RGB encoded), apply the color map
+        *  and write the same pixels in the output image out.
+        *  The output image must be of the same size of the input image.
+        *  The parameters d_x and d_y permit to evaluate the input image pixels every
+        *  d_x pixels along the x dimension and d_y pixels along y direction.
+        *  Return 0 if it fails, non-zero otherwise.
+        */
+	    int segment( const IplImage * in, const IplImage * out, int d_x = 1, int d_y = 1);
+	    
+	    /* 
+	    */
+	    inline CvScalar getRGB( const ColorClass c) { 
+	        return cvScalar(colorTable[c].b, colorTable[c].g, colorTable[c].r);
+	    }
+	    
+	    /*
+	    */
+	    inline bool isSeg( const ColorClass c ) { return colorTable[c].seg; }
+	    
+	    /* Load a color map from a file.
+	    */
+	//  bool readColorClassSpecifics( const string filename, unsigned int mergemod);
+    //  bool readColorClassSpecifics( const char * filename, unsigned int mergemod);
+        /* Save the color map into the specified file
+        */
+    //  int writeColorClassSpecifics( const string filename);
+    //  int writeColorClassSpecifics( const char * filename);
 
-	void clearSamples();
-	void clearSpecifics();
+    private:
 
-	ColorClass getColorClass( const CvScalar color);
-	bool addSample( const ColorClass colorClass, const CvScalar color);
-
-	CvScalar getRGB( const ColorClass colorClass) {
-		return cvScalar( colorTable[colorClass].b, colorTable[colorClass].g, colorTable[colorClass].r);
-	}
-
-//	bool load( const string filename);
-
-	int segment( const IplImage * in, const IplImage * out, int d_x = 1, int d_y = 1);
-
-	bool isSeg( const ColorClass c ) {
-		return colorTable[c].seg;
-	}
-
-	void setColorClassSpecifics( ColorClass id, CvScalar color, bool seg = true)
-	{
-		ColorClassSpecifics c;
-		c.seg = seg;
-		c.b = (int) color.val[0];
-		c.g = (int) color.val[1];
-		c.r = (int) color.val[2];
-
-		colorTable[id] = c;
-	}
-
-private:
-
-	typedef struct
-	{
-		bool seg;
-		int r, g, b;
-	} ColorClassSpecifics;
-
-	map< ColorClass, ColorClassSpecifics> colorTable;
-
-	struct kdtree * tree;
+	    typedef struct {
+		    bool seg;
+		    int r, g, b;    // R,G,B color values
+	    } ColorClassSpecifics;
+        
+        /* Map the ColorClass ids to the ColorClassSpecifics
+        */
+	    map< ColorClass, ColorClassSpecifics> colorTable;
+        /* Tree that keep the sequence of samples.
+        */
+	    struct kdtree * tree;
 };
 
 #endif  // __COLORTABLE_H__

4fee26d589ad63e23c75cb982098ba91fba1b4d9
enripicci <picci91@tiscali.it>
2014-10-08 00:27:12 +0200

    Commentato Blob.h/cpp, correzioni e migliorie.

--- obj_rec/src/Blob.cpp
+++ obj_rec/src/Blob.cpp
@@ -1,40 +1,31 @@
 #include "Blob.h"
+#include <cmath>
 
-//direct pixel access
-#define PIXEL(_img,_x,_y) (&((uchar*)(_img->imageData+ _img->widthStep* ( (_y < 0)? 0: ((_y >= _img->height)? _img->height -1: _y)) ))[ ( (_x < 0)? 0: ((_x >= _img->width)? _img->width -1: _x)) *_img->nChannels])
-
-
-Blob::Blob( ColorClass _color )
-{
+Blob::Blob( ColorClass _color ) {
+    color = _color;
 	top_left = cvPoint( 1000000, 1000000);
 	bottom_right = cvPoint( -1, -1);
 	w = cvPoint( 0, 0);
 	area = 0;
-	color = _color;
 }
 
-
-Blob::Blob( Run run, ColorClass _color, int d_y)
-{
+Blob::Blob( Run run, ColorClass _color, int d_y) {
+    color = _color;
 	top_left = cvPoint( 1000000, 1000000);
 	bottom_right = cvPoint( -1, -1);
 	w = cvPoint( 0, 0);
 	area = 0;
-	color = _color;
 
 	this->merge( run, d_y);
 }
 
 
-
-Blob::~Blob()
-{
+Blob::~Blob() {
 	clear();
 }
 
 
-void Blob::clear()
-{
+void Blob::clear() {
 	runs.clear();
 
 	top_left = cvPoint( 1000000, 1000000);
@@ -44,35 +35,45 @@ void Blob::clear()
 }
 
 
-bool Blob::isNear( Blob & blob, const int d_x, const int d_y)
-{
-	for( int i = 0; i < (int) this->runs.size(); i++)
-	for( int j = 0; j < (int) blob.runs.size(); j++)
-	{
-		if(fabs(this->runs[i].y - blob.runs[j].y) < d_y)
-		{
-			if(( this->runs[i].x -d_x <= blob.runs[j].x) &&
-				( this->runs[i].x + this->runs[i].len + d_x >= blob.runs[j].x) ) return true;
+bool Blob::isNear( Blob & blob, const int d_x, const int d_y) {
+    register int i;
+    register int j;
+    int diff_left, diff_right;
+    
+	for( i = 0; i < (int)runs.size(); i++) {
+	    for( j = 0; j < (int) blob.runs.size(); j++) {
+		    if( fabs(runs[i].y - blob.runs[j].y) < d_y) {
+		        
+		        diff_left = (runs[i].x - d_x) - (blob.runs[j].x + blob.runs[j].len);
+		        diff_right = (runs[i].x + runs[i].len + d_x) - (blob.runs[j].x);
+		        
+		        if((diff_left * diff_right) < 0)
+		            return true;
+		        return false;
+		        
+		    /*
+		        if(( runs[i].x -d_x <= blob.runs[j].x) &&
+				    ( runs[i].x + runs[i].len + d_x >= blob.runs[j].x) ) return true;
 
-			if(( this->runs[i].x -d_x <= blob.runs[j].x + blob.runs[j].len ) &&
-				( this->runs[i].x + this->runs[i].len + d_x >= blob.runs[j].x + blob.runs[j].len) ) return true;
+			    if(( this->runs[i].x -d_x <= blob.runs[j].x + blob.runs[j].len ) &&
+				    ( this->runs[i].x + this->runs[i].len + d_x >= blob.runs[j].x + blob.runs[j].len) ) return true;
 
-			if(( blob.runs[j].x -d_x <= this->runs[i].x) &&
-				( blob.runs[j].x + blob.runs[j].len + d_x >= this->runs[i].x) ) return true;
+			    if(( blob.runs[j].x -d_x <= this->runs[i].x) &&
+				    ( blob.runs[j].x + blob.runs[j].len + d_x >= this->runs[i].x) ) return true;
 
-			if(( blob.runs[j].x -d_x <= this->runs[i].x + this->runs[i].len ) &&
-				( blob.runs[j].x + blob.runs[j].len + d_x >= this->runs[i].x + this->runs[i].len) ) return true;
-		}
+			    if(( blob.runs[j].x -d_x <= this->runs[i].x + this->runs[i].len ) &&
+				    ( blob.runs[j].x + blob.runs[j].len + d_x >= this->runs[i].x + this->runs[i].len) ) return true;
+            */
+		    }
+	    }
 	}
 
 	return false;
 }
 
 
-Blob Blob::merge( Blob & blob, int d_y)
-{
-
-	vector < Run >::iterator it;
+Blob Blob::merge( Blob & blob, int d_y) {
+	vector <Run>::iterator it;
 	for( it = blob.runs.begin(); it != blob.runs.end(); it++)
 		this->merge(*it, d_y);
 
@@ -80,25 +81,34 @@ Blob Blob::merge( Blob & blob, int d_y)
 }
 
 
-bool Blob::merge( Run run, int d_y)
-{
+/// FIXME: what to do if two runs lay on the same line (same y coordinate) ?
+///        They should be merged into an unique run?
+bool Blob::merge( Run run, int d_y) {
+    register int i;
+
 	runs.push_back(run);
+	
+    if( false == runs.empty() ) {
+        // update the bbox
+	    if( run.x < top_left.x)
+		    top_left.x = run.x;
 
-	if( run.x < top_left.x)
-		top_left.x = run.x;
+	    if( run.y < top_left.y)
+		    top_left.y = run.y;
 
-	if( run.y < top_left.y)
-		top_left.y = run.y;
+	    if( (run.x + run.len) > bottom_right.x )
+		    bottom_right.x = run.x + run.len;
 
-	if( (run.x + run.len) > bottom_right.x )
-		bottom_right.x = run.x + run.len;
+	    if( run.y > bottom_right.y)
+		    bottom_right.y = run.y;
+    } else {
+        // just set the bbox
+        top_left.x = run.x;
+        top_left.y = bottom_right.y = run.y;
+        bottom_right.x = run.x + run.len;
+    }
 
-	if( run.y > bottom_right.y)
-		bottom_right.y = run.y;
-
-
-
-	for( int i = 0; i < run.len; i++)
+	for( i = 0; i < run.len; i++)
 		w.x += (run.x + i) *d_y;
 
 	w.y += run.y * run.len * d_y;
@@ -109,31 +119,37 @@ bool Blob::merge( Run run, int d_y)
 }
 
 
-CvSeq * Blob::getEdges()
-{
-	CvSeq* ptseq = cvCreateSeq( CV_SEQ_KIND_GENERIC|CV_32SC2, sizeof(CvContour),
-							sizeof(CvPoint), cvCreateMemStorage() ); 
+CvPoint Blob::getAvg() {
+    CvPoint avg;
+    avg.x = (int) (w.x/(float)area);
+    avg.y = (int) (w.y/(float)area);
+    return avg;
+}
 
-	vector < Run >::iterator it;
-	for( it = runs.begin(); it != runs.end(); it++)
-	{
-		CvPoint pt0;
-		pt0.x = (*it).x;
-		pt0.y = (*it).y;
-		cvSeqPush( ptseq, &pt0 );
 
-		CvPoint pt1;
-		pt1.x = (*it).x + (*it).len;
-		pt1.y = (*it).y;
-		cvSeqPush( ptseq, &pt1 );
+CvSeq * Blob::getEdges() {
+	CvSeq* ptseq = cvCreateSeq( CV_SEQ_KIND_GENERIC|CV_32SC2, 
+	                            sizeof(CvContour),
+							    sizeof(CvPoint),
+							    cvCreateMemStorage() ); 
+	CvPoint pt;
+	
+	for( vector <Run>::iterator it = runs.begin(); it != runs.end(); it++) {
+	    // left extreme
+		pt.x = it->x;
+		pt.y = it->y;
+		cvSeqPush( ptseq, &pt );
+		
+		// right extreme
+		pt.x += it->len;
+		cvSeqPush( ptseq, &pt );
 	}
 
 	return ptseq;
 }
 
 
-CvSeq * Blob::getConvexHull()
-{
+CvSeq * Blob::getConvexHull() {
 	CvSeq* ptseq = getEdges();
 
 	CvSeq * hull = cvConvexHull2( ptseq, cvCreateMemStorage(), CV_CLOCKWISE, 0 );
@@ -144,10 +160,9 @@ CvSeq * Blob::getConvexHull()
 }
 
 
-bool Blob::operator== ( Blob blob)
-{
+bool Blob::operator== ( const Blob &blob) {
 	if( (blob.color != this->color) )
-	 return false;
+        return false;
 
 /*
 	vector < Run  >::iterator it1;
@@ -161,74 +176,64 @@ bool Blob::operator== ( Blob blob)
 }
 
 
-list < Blob > Blob::extractBlobs( IplImage * image, ColorTable & colorTable, int d_x, int d_y)
-{
+list < Blob > Blob::extractBlobs( IplImage * image, ColorTable & colorTable, int d_x, int d_y) {
 	list < Blob > blobs;
+	list < Blob >::reverse_iterator it; // use reverse iterator for improve efficiency
+	Run run;
+	register int y;
+	register int x;
+	ColorClass c;
 
-	for( int y = 0; y < image->height; y += d_y)
-	for( int x = 0; x < image->width; x += d_x)
-	{
-		ColorClass c = colorTable.getColorClass( cvScalar( PIXEL(image,x,y)[0], PIXEL(image,x,y)[1], PIXEL(image,x,y)[2]) );
+	for( y = 0; y < image->height; y += d_y) {
+	    for( x = 0; x < image->width; x += d_x) {
+	    
+		    c = colorTable.getColorClass(image,x,y);
 
-		if(colorTable.isSeg(c))
-		{
-			//Creiamo la run
-			Run run;
-			run.x = x;
-			run.y = y;
-			run.len = 0;
-	
-			x+=d_x;
-			while( (c == colorTable.getColorClass( cvScalar( PIXEL(image,x,y)[0], PIXEL(image,x,y)[1], PIXEL(image,x,y)[2]) ) ) )
-			{
-				run.len += d_x;
-				if(x+d_x < image->width) x+=d_x;
-				else break;
-			}
+		    if( true == colorTable.isSeg(c) ) {
+			    // Create the run
+			    run.x = x;
+			    run.y = y;
+			    run.len = 0;
+			    
+			    for( x+=d_x ; (x < image->width) && (c == colorTable.getColorClass(image,x,y)) ; x+=d_x, run.len+=d_x) {
+			        // nothing to do here.
+			    }
 
-			//facciamo un Blob con quella run
-			Blob tmp( run, c, d_y + d_y /2);
-			blobs.push_front( tmp);
-			list < Blob >::iterator p_it = blobs.begin();
+			    // Create a blob and insert it into the list. It can be accessed with blobs.back() method.
+			    Blob tmp( run, c, d_y + d_y /2);
+			    blobs.push_back(tmp);
 
-			//vicinanza con gli altri blob dello stesso colore
-			list < Blob >::iterator it;
-			for( it = blobs.begin(); it != blobs.end(); it++)
-			{
-				//se c'è un blob vicino..
-				if( ((*it).getColorClass() == c) && ( (*it).isNear( *p_it, d_x + d_x /2, d_y + d_y /2)))
-				{
-					if(it != p_it)
-					{
-						(*it).merge(*p_it, d_y + d_y /2);
-						blobs.erase(p_it);
-						p_it = it;
-					}
-				}
-			}
-		}
+			    for( it = (blobs.rbegin() + 1); it != blobs.rend(); it++) {
+				    // merge near blobs
+				    if( (it->getColorClass() == c) && ( it->isNear(blobs.back(),d_x + d_x /2,d_y + d_y /2))) {
+					    it->merge(blobs.back(), d_y + d_y /2);
+					    blobs.pop_back();
+					    break;
+				    }
+			    }
+		    }
+	    }
 	}
 
 	return blobs;
 }
 
 
-void Blob::drawBlobs( ColorTable & colorTable, list< Blob > & blobs, IplImage * img, long minArea)
-{
+void Blob::drawBlobs( ColorTable & colorTable, list< Blob > & blobs, IplImage * img, long minArea) {
+    CvSeq * hull
+    CvPoint pt0, pt1;
 	list< Blob >::iterator b_it;
-	for( b_it = blobs.begin(); b_it != blobs.end(); b_it++)
-	{
-		Blob b = *b_it;
+	
+	for( b_it = blobs.begin(); b_it != blobs.end(); b_it++) {
 
-		if(b.getArea() > minArea)
-		{
-			CvSeq * hull = b.getConvexHull();
+		if(b_it->getArea() > minArea) {
+			hull = b_it->getConvexHull();
 
-			for( int i = 0; i < hull->total; i++ )
-			{
-				CvPoint pt0, pt1;
+			for( int i = 0; i < hull->total; i++ ) {
 				pt0 = **CV_GET_SEQ_ELEM( CvPoint *, hull, i );
 				pt1 = **CV_GET_SEQ_ELEM( CvPoint *, hull, (i+1)%(hull->total) );
+				
+				// paint something
 				cvCircle( img, pt0, 2, colorTable.getRGB(b.color));
 				cvLine( img, pt0, pt1, colorTable.getRGB(b.color), 1);
 			}
@@ -241,15 +246,11 @@ void Blob::drawBlobs( ColorTable & colorTable, list< Blob > & blobs, IplImage * 
 }
 
 
-void Blob::deleteBlobs( list< Blob > & blobs)
-{
+void Blob::deleteBlobs( list< Blob > & blobs) {
 	list< Blob >::iterator b_it;
-	for( b_it = blobs.begin(); b_it != blobs.end(); b_it++)
-	{
-		Blob b = *b_it;
-
-		b.clear();
+	for( b_it = blobs.begin(); b_it != blobs.end(); b_it++) {
+		b_it->clear();
 	}
 
-	blobs.clear();
+	blobs.clear();  // free the list
 }
--- obj_rec/src/Blob.h
+++ obj_rec/src/Blob.h
@@ -4,77 +4,116 @@
 #include <map>
 #include <vector>
 #include <list>
-using namespace std;
-
 // OpenCV Lib
 #include <cv.h>
 #include <cxcore.h>
 
 #include "ColorTable.h"
 
-class Blob
-{
-public:
+using namespace std;
 
-static list < Blob > extractBlobs( IplImage * image, ColorTable & colorTable, int d_x = 3, int d_y = 3);
-static void drawBlobs( ColorTable & colorTable, list< Blob > & blobs, IplImage * img, long minArea = 1000);
-static void deleteBlobs( list< Blob > & blobs);
+/*
+*/
+class Blob {
+    public:
+        /* Create a blob that track pixels with color of class colorId
+        */
+        Blob( ColorClass colorId);
+	    virtual ~Blob();
+        
+        /* Extract the blobs from the image image, according to the colorId rules
+        *  defined in colorTable.
+        *  The image's pixels are evaluated every d_x pixel along x and every d_y along y.
+        */
+        static list < Blob > extractBlobs( IplImage * image, ColorTable & colorTable, int d_x = 3, int d_y = 3);
+        
+        /* Draw the ConvexHull of every blobs in the list and their center into the image img.
+        *  Blobs with area lesser than minArea are excluded.
+        */
+        static void drawBlobs( ColorTable & colorTable, list< Blob > & blobs, IplImage * img, long minArea = 1000);
+        
+        /* Call clear() on every blob in the supplied list.
+        *  The blob list is freed after use.
+        */
+        static void deleteBlobs( list< Blob > & blobs);
+        
+        /* Clear all the runs in the blob.
+        */
+	    void clear();
+        
+        /**    Bounding box
+        **/
+        /* Top-left point of bbox
+        */
+	    inline CvPoint getTopLeft() const { return top_left; }
+	    
+	    /* Bottom-right point of bbox
+        */
+	    inline CvPoint getBottomRight() const { return bottom_right; }
+	    
+	    /* Bbox rectangle
+        */
+	    inline CvRect getBoundingBox() {
+	        return cvRect(top_left.x, top_left.y, bottom_right.x - top_left.x, bottom_right.y - top_left.y);
+	    }
 
-	Blob( ColorClass _color);
-	virtual ~Blob();
+	    CvPoint getAvg();
 
-	void clear();
+	    long getArea() const { return area; }
+        
+        /* Return true if blob has some (1 or more) runs that share a border or
+        *  overlap.
+        *  d_x and d_y are the pixel of tolerance along X and Y.
+        */
+	    bool isNear( Blob & blob, const int d_x = 2, const int d_y = 2);
+        
+        /* Convex hull
+        */
+	    CvSeq * getConvexHull();
+	    
+	    /* Create a sequence of extremity points
+	    */
+	    CvSeq * getEdges();
+        
+        /* ColorId tracked in the blob
+        */
+	    inline ColorClass getColorClass() const { return color; }
+        
+        /* Two blobs are equal if they track the same colorId
+        */
+	    bool operator== ( const Blob &blob);
 
-	CvPoint getTopLeft() const {
-		return top_left;
-	}
+    private:
+        
+        /* A Run is an horizontal line of pixels with same attributes, such as
+        *  color, like in this case.
+        */
+	    typedef struct {
+		    int x, y;
+		    int len;
+	    } Run;
 
-	CvPoint getBottomRight() const {
-		return bottom_right;
-	}
+	    ColorClass color;
+	    CvPoint top_left;
+	    CvPoint bottom_right;
+	    CvPoint w;
+	    long area;
+        
+        /* Create a Blob and initialize it with the supplied run
+        */
+	    Blob( Run run, ColorClass _color, int d_y);
+	    
+	    /* Add a new run.
+	    *  This call can change the bounding box.
+	    *  Return true on success.
+	    */
+	    bool merge( Run run, int d_y);
+	    
+	    /* Merge all the Runs of blob with the current one.
+	    */
+	    Blob merge( Blob & blob, int d_y);
 
-	CvPoint getAvg() {
-		CvPoint avg;
-		avg.x = (int) (w.x/(float)area);
-		avg.y = (int) (w.y/(float)area);
-		return avg;
-	}
-
-	long getArea() const {
-		return area;
-	}
-
-	bool isNear( Blob & blob, const int d_x = 2, const int d_y = 2);
-
-	CvSeq * getConvexHull();
-	CvSeq * getEdges();
-
-	ColorClass getColorClass() {
-		return color;
-	}
-
-	bool operator== ( Blob blob);
-
-private:
-	typedef struct
-	{
-		int x, y;
-		int len;
-	} Run;
-
-	ColorClass color;
-
-	CvPoint top_left;
-	CvPoint bottom_right;
-
-	CvPoint w;
-	long area;
-
-	Blob( Run run, ColorClass _color, int d_y);
-	bool merge( Run run, int d_y);
-	Blob merge( Blob & blob, int d_y);
-
-	vector < Run > runs;
+	    vector < Run > runs;
 };
 
 #endif  //__BLOB_H__ 
